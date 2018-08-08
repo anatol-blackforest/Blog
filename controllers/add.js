db93536033533c46ed1dbb18ac4c7d06e2972b5d
anatol-blackforest <anatolblackforest@gmail.com>
2018-08-08 20:54:57 +0300

    error page

--- controllers/add.js
+++ controllers/add.js
@@ -5,7 +5,7 @@ module.exports = async (req, res) => {
         const category = await Category.findOne({name: req.body.category})
         if (!category) await Category.create({name: req.body.category})
         const categories = await Category.find()
-        return res.status(200).json({post, categories});
+        return res.status(201).json({post, categories});
     }catch(err) {
         console.error('Unable to connect to the database:', err);
         return res.status(500).json({error: 500});
